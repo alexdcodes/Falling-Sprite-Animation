16acb95f257ee3d2ef8fefe30d353d3a2916d0a3
Alex Diker <alex.diker@live.ca>
2020-08-08 12:41:57 -0400

    Add files via upload

--- weights.py
+++ weights.py
@@ -6,4 +6,17 @@ class Weight(pygame.sprite.Sprite)
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        self.image = weight_image
+        self.rect = self.image.get_rect()
+        self.reset()
+
+    def reset(self):
+        """
+
+        :return:
+        """
+        self.rect.top = -self.rect.height
+        self.rect.centerx = randrange(screen_size[0])
+
+    def update(self):
         
