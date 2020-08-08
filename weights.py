c345c96a98b3e7054c69824b4821d53b19e70476
Alex Diker <alex.diker@live.ca>
2020-08-08 12:39:42 -0400

    Add files via upload

--- weights.py
+++ weights.py
@@ -1,2 +1,9 @@
 import sys, pygame
 from pygame.locals import *
+from random import randrange
+
+class Weight(pygame.sprite.Sprite)
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        
